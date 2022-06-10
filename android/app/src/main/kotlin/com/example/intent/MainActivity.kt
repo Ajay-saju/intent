84943bedab46e13bab65e727c7a5efda871e00fe
a <you@example.com>
2022-06-10 11:27:14 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/intent/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.intent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/screens/signup.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Column(children: [
+
+      ],)),
+    );
+    
+  }
+}
