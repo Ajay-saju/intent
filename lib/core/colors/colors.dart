664682fcc7f201327e544ade915815e204863138
a <you@example.com>
2022-06-10 20:17:42 +0530

    next commit

---
+++ lib/core/colors/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/cupertino.dart';
+
+// const textColor = Color.fromARGB(a, r, g, b)
--- lib/view/screens/signup.dart
+++ lib/view/screens/signup.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({Key? key}) : super(key: key);
@@ -9,6 +11,10 @@ class SignupScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(child: Column(children: [
+        Padding(
+          padding:  EdgeInsets.only(left: 1.h),
+          child: AppBar(backgroundColor: Colors.transparent,elevation: 0,toolbarHeight: 15.h, title: Text('Register !',style: GoogleFonts.aBeeZee(textStyle:TextStyle(color: Colors.black,letterSpacing: 2,fontSize: 25.sp,fontWeight: FontWeight.bold)),),),
+        )
 
       ],)),
     );
